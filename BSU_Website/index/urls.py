c89959d1736795cedd7c20dd6f797f8eb336745a
Kobena Idun <plasmadeveloper@outlook.com>
2020-06-10 17:02:42 -0400

    added url mappings

--- BSU_Website/index/urls.py
+++ BSU_Website/index/urls.py
@@ -4,5 +4,9 @@ from . import views
 app_name = "index"
 
 urlpatterns = [
-    path('', views.homepage, name = "homepage"),    
+    path('', views.homepage, name = "homepage"),
+    path('action/',views.actionpage, name = "actionpage"),
+    path('blm/',views.blmpage, name = "blmpage"), 
+    path('leaders/',views.leaderspage, name = "leaderspage"),
+    path('racism/',views.racismpage, name = "racismpage"),
 ]
--- BSU_Website/index/views.py
+++ BSU_Website/index/views.py
@@ -3,3 +3,11 @@ from django.http import HttpResponse
 # Create your views here.
 def homepage(request):
     return render(request,"index.html")
+def actionpage(request):
+    return render(request,"action.html")
+def blmpage(request):
+    return render(request,"blm.html")
+def leaderspage(request):
+    return render(request,"leaders.html")
+def racismpage(request):
+    return render(request,"racism.html")
